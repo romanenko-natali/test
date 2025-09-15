dfa5052dadb2f34d144f3a7462378043bad35a96
nataliia <nromanen.softserveinc@gmail.com>
2025-09-15 15:49:53 +0100

    initial project

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,24 @@
-def 
+import pytest
+from src.main import add
+
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+
+def test_add_negative_numbers():
+    assert add(-2, -3) == -5
+
+
+def test_add_mixed_numbers():
+    assert add(-2, 3) == 1
+
+
+def test_add_zero():
+    assert add(0, 5) == 5
+    assert add(0, 0) == 0
+
+
+def test_add_raises():
+    with pytest.raises(TypeError):
+        add("a", 3)
